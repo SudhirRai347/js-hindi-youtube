6e9a77bff2d17abf958b589513cb2e5cdaecdcc2
SudhirRai347 <raisudhir227@gmail.com>
2025-06-28 10:25:39 +0000

    StringinJS

---
+++ 01_basics/05_string.js
@@ -0,0 +1,14 @@
+const name="hitesh";
+const repoCount=30;
+//console.log(name+repoCount+" value");
+console.log(`My name is ${name} and my repo count is ${repoCount}`);
+
+const gameName=new String('hiteshic');
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
