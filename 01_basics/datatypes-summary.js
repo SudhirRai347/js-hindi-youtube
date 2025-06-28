ec5528c128ecf99e293b3e33e469a1382bcc5bc2
SudhirRai347 <raisudhir227@gmail.com>
2025-06-28 09:26:03 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,28 @@
+// Primitive
+ // 7 types: String,Number,boolean,null,undefined,Symbol,BigInt
+const score=100;
+const scoreValue=100.3
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail  //undefined type
+
+const id=Symbol('123');
+const anotherId=Symbol('123');
+console.log(id===anotherId);
+
+// const bigInt=324567891n;
+
+
+// Reference(non Prirmitive)
+ // Array,Objects,Functions 
+
+ const heros=["Ramji","Ganeshji","Hanumanji"];
+ let myObj={
+    name: "Sudhir",
+    age: "21",
+ }
+ const myFunction=function(){
+    console.log("Jai shree Ram");
+ }
+
+ console.log(typeof(id));
