df83393bcef46926ed72aa2c70ffd3e8ba21d1e4
SudhirRai347 <raisudhir227@gmail.com>
2025-06-29 10:01:33 +0000

    NumbersAndMaths

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -4,11 +4,27 @@ const repoCount=30;
 console.log(`My name is ${name} and my repo count is ${repoCount}`);
 
 const gameName=new String('hiteshic');
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
 
-console.log(gameName.length);
-console.log(gameName.toUpperCase());
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
 
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('t'));
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+const newString=gameName.substring(0,4)
+console.log(newString);
+
+const anotherString=gameName.slice(-8,4);
+console.log(anotherString);
+
+const newStringOne="  JaiShreeRam  ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url="https://hitesh.com/hitesh%20Choudary";
+console.log(url.replace('%20','--'));
+
+console.log(url.includes('hitesh'));
+console.log(gameName.split('--'));
