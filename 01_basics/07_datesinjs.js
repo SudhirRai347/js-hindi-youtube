4c7e00f78eb60ad73940b1a8e9d5a9238b633631
SudhirRai347 <raisudhir227@gmail.com>
2025-06-29 12:54:58 +0000

    Datein js

---
+++ 01_basics/07_datesinjs.js
@@ -0,0 +1,23 @@
+//Dates
+let myDate=new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+//console.log(typeof myDate);
+
+//let myCreatedDate=new Date(2025,0,25);
+//let myCreateDate=new Date(2025,0,25,5,3);
+//let myCreateDate=new Date("2025-01-14");
+//console.log(myCreatedDate);
+let myCreateDate=new Date("02-14-2023");
+//console.log(myCreateDate.toDateString());
+
+let myTimeStamp=Date.now();
+//console.log(myTimeStamp);
+//console.log(myCreateDate.getTime());
+
+//console.log(Math.floor(Date.now()/1000));
+let newDate=new Date();
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
