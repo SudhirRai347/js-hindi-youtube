fe67b53783c9108e82e1d80034891431b3d0e935
SudhirRai347 <raisudhir227@gmail.com>
2025-07-02 12:53:37 +0000

    forinloop

---
+++ 05_iterations/four.js
@@ -0,0 +1,14 @@
+const myObject={
+    js: 'javaScript',
+    cpp: 'C++',
+    rb: "rubby",
+    swift: "Swift by apple"
+}
+ for(const key in myObject){
+    console.log(`${key} shortcut is for ${myObject[key]}`);
+ }
+
+//  const programming=["js","C++","rb","Swift","java"];
+//  for(const key in programming){
+//     console.log(programming[key]);
+//  }
